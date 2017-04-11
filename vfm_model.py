af28e29fcf5626dd703f27822806e7dd58d8b042
sean <seann999@gmail.com>
2017-04-12 07:16:13 +0900

    basic training 2

--- vfm_model.py
+++ vfm_model.py
@@ -1,14 +1,59 @@
 import tensorflow as tf
-import tensorflow.contrib.layers as layers
+from tensorflow.contrib import layers
+from tensorflow.contrib import rnn
 
 class Model:
-    def __init__(self):
-        observation = tf.placeholder(tf.float32, [None, 7, 7, 1])
+    def __init__(self, frame_size, n_steps):
+        self.observation = tf.placeholder(tf.float32, [None, n_steps, frame_size, frame_size, 1])
+        self.target = tf.placeholder(tf.float32, [None, n_steps, frame_size, frame_size, 1])
+        self.locations = tf.placeholder(tf.float32, [None, n_steps, 2])
 
-        h = layers.convolution2d(observation, 32, [4, 4], activation_fn=tf.nn.elu)
-        h = layers.flatten(h)
-        h_mu = layers.fully_connected(h, 10, activation_fn=None)
-        h_sigma = layers.fully_connected(h, 10, activation_fn=None)
-        z = h_mu + tf * h_sigma
+        observation = tf.reshape(self.observation, [-1, frame_size**2])
+        target = tf.reshape(self.target, [-1, frame_size ** 2])
+        #h = layers.convolution2d(observation, 32, [4, 4], activation_fn=tf.nn.elu)
+        #h = layers.flatten(h)
+        h = observation
 
-    
+        h_size = 128
+        cell_size = 128
+        act = tf.nn.relu
+
+        h = layers.fully_connected(h, h_size, activation_fn=act)
+        h = layers.fully_connected(h, h_size, activation_fn=act)
+
+        h = tf.reshape(h, [-1, n_steps, h_size])
+        h = tf.unstack(h, n_steps, 1)
+        lstm_cell = rnn.BasicLSTMCell(cell_size, forget_bias=1.0)
+        outputs, states = rnn.static_rnn(lstm_cell, h, dtype=tf.float32)
+        h = tf.stack(outputs, 1)
+
+        h = tf.reshape(h, [-1, cell_size])
+
+        n_z = 32
+
+        z_mean = layers.fully_connected(h, n_z, activation_fn=None)
+        z_logvar = layers.fully_connected(h, n_z, activation_fn=None)
+
+        eps = tf.random_normal(tf.shape(z_mean), 0, 1, dtype=tf.float32)
+        self.z = tf.add(z_mean,
+                        tf.multiply(tf.sqrt(tf.exp(z_logvar)), eps))
+
+        #self.z_test = layers.fully_connected(tf.stop_gradient(h), frame_size**2, activation_fn=None)
+        #self.class_test =
+
+        locations = tf.reshape(self.locations, [-1, 2])
+        h = tf.concat([self.z, locations], 1)
+
+        h = layers.fully_connected(h, h_size, activation_fn=act)
+        h = layers.fully_connected(h, h_size, activation_fn=act)
+        self.reconstruction = layers.fully_connected(h, frame_size**2, activation_fn=None)
+        self.reconstruction2d = tf.reshape(self.reconstruction, [-1, frame_size, frame_size, 1])
+
+        BCE = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.reconstruction, labels=target)
+        KLD = -0.5 * (1 + z_logvar - tf.pow(z_mean, 2) - tf.exp(z_logvar))
+
+        BCE = tf.reduce_sum(BCE, axis=1)
+        KLD = tf.reduce_sum(KLD, axis=1)
+
+        self.loss = tf.reduce_mean(BCE + KLD)
+        self.optimizer = tf.train.AdamOptimizer(1e-3).minimize(self.loss)
--- vfm_trainer.py
+++ vfm_trainer.py
@@ -1,14 +1,78 @@
 from tensorflow.examples.tutorials.mnist import input_data
-#import cv2
+import cv2
 import vfm_model
+import tensorflow as tf
+import numpy as np
 
 mnist = input_data.read_data_sets("data", False)
 
-print(mnist.train.images.shape)
-print(mnist.train.labels.shape)
+frame = 14
+steps = 10
+batch_size = 32
 
-images, labels = mnist.train.next_batch(8)
+model = vfm_model.Model(frame, steps)
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
-#cv2.imshow("image", images[0])
+cv2.namedWindow("images", cv2.WINDOW_NORMAL)
+cv2.namedWindow("gen", cv2.WINDOW_NORMAL)
 
-model = vfm_model.Model()
+for i in range(10000000):
+    images, labels = mnist.train.next_batch(batch_size)
+
+    instances = []
+    locations = []
+
+    for image in images:
+        glimpses = []
+        image = np.reshape(image, [28, 28])
+
+        for k in range(steps+1):
+            x, y = np.random.randint(low=0, high=28-frame, size=2)
+            glimpses.append(image[y:y+frame, x:x+frame])
+
+            if k > 0:
+                locations.append([y, x])
+
+        instances.append(glimpses)
+
+    images = np.reshape(instances, [batch_size, steps+1, frame, frame, 1])
+    locations = np.reshape(locations, [batch_size, steps, 2])
+
+    _, loss, rec = sess.run([model.optimizer, model.loss, model.reconstruction2d],
+                            feed_dict={
+                                model.observation: images[:, :-1, :, :, :],
+                                model.target: images[:, 1:, :, :, :],
+                                model.locations: locations
+                            })
+
+    if i % 1000 == 0:
+        print(i, loss)
+
+        inputs, targets, predictions = [], [], []
+
+        for k in range(steps):
+            inputs.append(images[0, k, :, :, :])
+            targets.append(images[0, k+1, :, :, :])
+            predictions.append(rec[k])
+
+        inputs, targets, predictions = np.hstack(inputs), np.hstack(targets), np.hstack(predictions)
+
+        cv2.imshow("images", np.vstack([inputs, targets, predictions]).astype(np.float32))
+        cv2.waitKey(1)
+
+    """
+    if i % 1000 == 0:
+        nx = ny = 20
+        x_values = np.linspace(-3, 3, nx)
+        y_values = np.linspace(-3, 3, ny)
+        canvas = np.empty((frame * ny, frame * nx))
+        for i, yi in enumerate(x_values):
+            for j, xi in enumerate(y_values):
+                z_mu = [xi, yi]
+                x_mean = sess.run(model.reconstruction, feed_dict={model.z: [z_mu] * 5, model.locations: np.zeros([1, 5, 2])})
+                canvas[(nx - i - 1) * frame:(nx - i) * frame, j * frame:(j + 1) * frame] = x_mean[0].reshape(frame, frame)
+
+        cv2.imshow("gen", canvas)
+        cv2.waitKey(1)
+    """
