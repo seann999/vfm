bb1672059282e5390cac84721d6c6f8efea8d0c6
sean <seann999@gmail.com>
2017-04-12 15:20:07 +0900

    img completion

--- vfm_model.py
+++ vfm_model.py
@@ -4,18 +4,22 @@ from tensorflow.contrib import rnn
 
 class Model:
     def __init__(self, frame_size, n_steps):
+        self.global_step = tf.Variable(0)
         self.observation = tf.placeholder(tf.float32, [None, n_steps, frame_size, frame_size, 1])
         self.target = tf.placeholder(tf.float32, [None, n_steps, frame_size, frame_size, 1])
-        self.locations = tf.placeholder(tf.float32, [None, n_steps, 2])
+        self.true_label = tf.placeholder(tf.int32, [None])
+        self.locations = tf.placeholder(tf.float32, [None, n_steps+1, 2])
 
         observation = tf.reshape(self.observation, [-1, frame_size**2])
+        observation = tf.concat([observation, tf.reshape(self.locations[:, :-1, :], [-1, 2])], 1)
+
         target = tf.reshape(self.target, [-1, frame_size ** 2])
         #h = layers.convolution2d(observation, 32, [4, 4], activation_fn=tf.nn.elu)
         #h = layers.flatten(h)
         h = observation
 
-        h_size = 128
-        cell_size = 128
+        h_size = 512
+        cell_size = 512
         act = tf.nn.relu
 
         h = layers.fully_connected(h, h_size, activation_fn=act)
@@ -29,7 +33,7 @@ class Model:
 
         h = tf.reshape(h, [-1, cell_size])
 
-        n_z = 32
+        n_z = 10
 
         z_mean = layers.fully_connected(h, n_z, activation_fn=None)
         z_logvar = layers.fully_connected(h, n_z, activation_fn=None)
@@ -38,22 +42,33 @@ class Model:
         self.z = tf.add(z_mean,
                         tf.multiply(tf.sqrt(tf.exp(z_logvar)), eps))
 
-        #self.z_test = layers.fully_connected(tf.stop_gradient(h), frame_size**2, activation_fn=None)
-        #self.class_test =
+        self.z_test = layers.fully_connected(tf.stop_gradient(self.z), 10, activation_fn=None)
+        self.class_test = tf.nn.softmax(self.z_test)
 
-        locations = tf.reshape(self.locations, [-1, 2])
+        locations = tf.reshape(self.locations[:, 1:, :], [-1, 2])
         h = tf.concat([self.z, locations], 1)
 
         h = layers.fully_connected(h, h_size, activation_fn=act)
         h = layers.fully_connected(h, h_size, activation_fn=act)
-        self.reconstruction = layers.fully_connected(h, frame_size**2, activation_fn=None)
-        self.reconstruction2d = tf.reshape(self.reconstruction, [-1, frame_size, frame_size, 1])
+        self.prediction = layers.fully_connected(h, frame_size ** 2, activation_fn=None)
+        self.prediction2d = tf.reshape(self.prediction, [-1, frame_size, frame_size, 1])
 
-        BCE = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.reconstruction, labels=target)
+        self.BCE = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.prediction, labels=target)
+        self.BCE = tf.reduce_sum(self.BCE, axis=1)
+
+        self.avg_BCE_dec = tf.reshape(self.BCE, [-1, n_steps])
+        self.avg_BCE_dec = tf.reduce_mean(self.avg_BCE_dec[:, :-1] - self.avg_BCE_dec[:, 1:])
+
+        self.BCE = tf.reduce_mean(self.BCE)
+
         KLD = -0.5 * (1 + z_logvar - tf.pow(z_mean, 2) - tf.exp(z_logvar))
+        KLD = tf.reduce_mean(tf.reduce_sum(KLD, axis=1))
 
-        BCE = tf.reduce_sum(BCE, axis=1)
-        KLD = tf.reduce_sum(KLD, axis=1)
+        class_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.z_test, labels=self.true_label)
 
-        self.loss = tf.reduce_mean(BCE + KLD)
-        self.optimizer = tf.train.AdamOptimizer(1e-3).minimize(self.loss)
+        self.avg_ce_dec = tf.reshape(class_loss, [-1, n_steps])
+        self.avg_ce_dec = tf.reduce_mean(self.avg_ce_dec[:, :-1] - self.avg_ce_dec[:, 1:])
+        self.class_acc = tf.reduce_mean(tf.cast(tf.equal(self.true_label, tf.cast(tf.argmax(self.z_test, 1), tf.int32)), tf.float32))
+
+        self.loss = KLD + self.BCE + tf.reduce_mean(class_loss)
+        self.optimizer = tf.train.AdamOptimizer(1e-3).minimize(self.loss, global_step=self.global_step)
--- vfm_trainer.py
+++ vfm_trainer.py
@@ -3,76 +3,162 @@ import cv2
 import vfm_model
 import tensorflow as tf
 import numpy as np
+import time
 
-mnist = input_data.read_data_sets("data", False)
+np.random.seed(1)
+tf.set_random_seed(1)
 
-frame = 14
-steps = 10
-batch_size = 32
+flags = tf.app.flags
+FLAGS = flags.FLAGS
 
-model = vfm_model.Model(frame, steps)
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+def summary_float(step, name, value, summary_writer):
+    summary = tf.Summary(
+        value=[tf.Summary.Value(tag=name, simple_value=float(value))])
+    summary_writer.add_summary(summary, global_step=step)
 
-cv2.namedWindow("images", cv2.WINDOW_NORMAL)
-cv2.namedWindow("gen", cv2.WINDOW_NORMAL)
+def run():
+    mnist = input_data.read_data_sets("data", False)
 
-for i in range(10000000):
-    images, labels = mnist.train.next_batch(batch_size)
+    frame = FLAGS.glimpse_size
+    steps = FLAGS.time_steps
+    batch_size = FLAGS.batch_size
 
-    instances = []
-    locations = []
+    model = vfm_model.Model(frame, steps)
 
-    for image in images:
-        glimpses = []
-        image = np.reshape(image, [28, 28])
+    saver = tf.train.Saver(max_to_keep=5)
+    sess = tf.Session()
+    writer = tf.summary.FileWriter(FLAGS.model_dir)
+    sess.run(tf.global_variables_initializer())
 
-        for k in range(steps+1):
-            x, y = np.random.randint(low=0, high=28-frame, size=2)
-            glimpses.append(image[y:y+frame, x:x+frame])
+    ckpt = tf.train.get_checkpoint_state(FLAGS.model_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        saver.restore(sess, ckpt.model_checkpoint_path)
+        print("restored %s" % ckpt.model_checkpoint_path)
 
-            if k > 0:
-                locations.append([y, x])
+    cv2.namedWindow("images", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("gen", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("extrapolate", cv2.WINDOW_NORMAL)
 
-        instances.append(glimpses)
+    while True:
+        global_step = sess.run(model.global_step)
 
-    images = np.reshape(instances, [batch_size, steps+1, frame, frame, 1])
-    locations = np.reshape(locations, [batch_size, steps, 2])
+        def learn(images, labels, train):
+            instances = []
+            locations = []
+            true_labels = []
 
-    _, loss, rec = sess.run([model.optimizer, model.loss, model.reconstruction2d],
-                            feed_dict={
-                                model.observation: images[:, :-1, :, :, :],
-                                model.target: images[:, 1:, :, :, :],
-                                model.locations: locations
-                            })
+            for image, label in zip(images, labels):
+                glimpses = []
+                image = np.reshape(image, [28, 28])
 
-    if i % 1000 == 0:
-        print(i, loss)
+                for k in range(steps+1):
+                    x, y = np.random.randint(low=0, high=28-frame, size=2)
+                    glimpses.append(image[y:y+frame, x:x+frame])
+                    locations.append([y, x])
 
-        inputs, targets, predictions = [], [], []
+                    if k > 0:
+                        true_labels.append(label)
 
-        for k in range(steps):
-            inputs.append(images[0, k, :, :, :])
-            targets.append(images[0, k+1, :, :, :])
-            predictions.append(rec[k])
+                instances.append(glimpses)
 
-        inputs, targets, predictions = np.hstack(inputs), np.hstack(targets), np.hstack(predictions)
+            images = np.reshape(instances, [batch_size, steps+1, frame, frame, 1])
+            locations = np.reshape(locations, [batch_size, steps+1, 2])
+            fd = {model.observation: images[:, :-1, :, :, :],
+                  model.target: images[:, 1:, :, :, :],
+                  model.locations: locations,
+                  model.true_label: true_labels
+                  }
 
-        cv2.imshow("images", np.vstack([inputs, targets, predictions]).astype(np.float32))
-        cv2.waitKey(1)
+            if train:
+                global_step, _, loss, rec, class_p, img_error, dec, class_acc, avg_ce_dec = sess.run([model.global_step, model.optimizer, model.loss, model.prediction2d, model.class_test,
+                                                                                                      model.BCE, model.avg_BCE_dec, model.class_acc, model.avg_ce_dec],
+                                        feed_dict=fd)
 
-    """
-    if i % 1000 == 0:
-        nx = ny = 20
-        x_values = np.linspace(-3, 3, nx)
-        y_values = np.linspace(-3, 3, ny)
-        canvas = np.empty((frame * ny, frame * nx))
-        for i, yi in enumerate(x_values):
-            for j, xi in enumerate(y_values):
-                z_mu = [xi, yi]
-                x_mean = sess.run(model.reconstruction, feed_dict={model.z: [z_mu] * 5, model.locations: np.zeros([1, 5, 2])})
-                canvas[(nx - i - 1) * frame:(nx - i) * frame, j * frame:(j + 1) * frame] = x_mean[0].reshape(frame, frame)
+                summary_float(global_step, "train loss", loss, writer)
+                summary_float(global_step, "train image error", img_error, writer)
+                summary_float(global_step, "train image error decrease", dec, writer)
+                summary_float(global_step, "train z class accuracy", class_acc, writer)
+                summary_float(global_step, "train cross entropy decrease", avg_ce_dec, writer)
 
-        cv2.imshow("gen", canvas)
-        cv2.waitKey(1)
-    """
+                if global_step % 100 == 0:
+                    print(global_step, loss)
+
+                if global_step % 10000 == 0:
+                    saver.save(sess, FLAGS.model_dir + "/model", global_step)
+            else:
+                global_step = sess.run(model.global_step)
+                rec, class_p, img_error, dec, class_acc, avg_ce_dec = sess.run([model.prediction2d, model.class_test, model.BCE, model.avg_BCE_dec, model.class_acc, model.avg_ce_dec],
+                                                                               feed_dict=fd)
+                summary_float(global_step, "test image error", img_error, writer)
+                summary_float(global_step, "test image error decrease", dec, writer)
+                summary_float(global_step, "test z class accuracy", class_acc, writer)
+                summary_float(global_step, "test cross entropy decrease", avg_ce_dec, writer)
+
+                inputs, targets, predictions = [], [], []
+
+                for k in range(steps):
+                    inputs.append(images[0, k, :, :, :])
+                    targets.append(images[0, k+1, :, :, :])
+                    predictions.append(rec[k])
+                    print("%i: %f" % (np.argmax(class_p[k]), np.amax(class_p[k])))
+
+                inputs, targets, predictions = np.hstack(inputs), np.hstack(targets), np.hstack(predictions)
+
+                cv2.imshow("images", np.vstack([inputs, targets, predictions]).astype(np.float32))
+                cv2.waitKey(1)
+
+                print("-"*10)
+
+            return global_step
+
+        train = False
+
+        if train:
+            images, labels = mnist.train.next_batch(batch_size)
+            learn(images, labels, True)
+
+            if global_step % 100 == 0:
+                images, labels = mnist.test.next_batch(batch_size)
+                learn(images, labels, False)
+
+        if not train and global_step % 1000 == 0:
+            time.sleep(5)
+            images, labels = mnist.test.next_batch(batch_size)
+            image = images[0]
+
+            locations = [[0, 14], [14, 0], [14, 14]]
+            image = np.reshape(image, [28, 28, 1])
+            patches = []
+
+            for x, y in locations:
+                glimpses = []
+                glimpses.append(image[0:frame, 0:frame])
+                glimpses.append(image[y:y+frame, x:x+frame])
+                images = np.reshape(glimpses, [batch_size, 2, frame, frame, 1])
+                locations = np.reshape([[0, 0], [x, y]], [batch_size, 2, 2])
+                fd = {model.observation: images[:, :-1, :, :, :],
+                      model.target: images[:, 1:, :, :, :],
+                      model.locations: locations
+                      }
+                rec = sess.run(
+                    model.prediction2d,
+                    feed_dict=fd)
+                patches.append(rec[0])
+
+            whole = np.empty([28, 28, 1])
+            image[:frame, :frame] = 1.0 - image[:frame, :frame]
+            whole[:14, :14] = image[:frame, :frame]
+            whole[:14, 14:] = patches[0]
+            whole[14:, :14] = patches[1]
+            whole[14:, 14:] = patches[2]
+
+            cv2.imshow("extrapolate", np.hstack([whole, image]))
+            cv2.waitKey(1)
+
+if __name__ == "__main__":
+    flags.DEFINE_string("model_dir", "summaries/test00", "model dir")
+    flags.DEFINE_integer("batch_size", 32, "batch size")
+    flags.DEFINE_integer("time_steps", 10, "time steps")
+    flags.DEFINE_integer("glimpse_size", 14, "glimpse_size")
+
+    run()
